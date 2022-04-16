342bf4335674a01aae64b7446ae86a5e3f2312d9
TristanAnjay <102612033+TristanAnjay@users.noreply.github.com>
2022-04-16 09:13:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.owner = ['6281290867594'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = [628977884828] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
